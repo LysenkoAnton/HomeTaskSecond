a679faf16c55c1f8be2d9c078e02b2fbce9d8e45
Lysenko Anton <tony@i.ua>
2019-04-21 18:30:58 +0300

    created stucts in alone file, realized examples using

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group groupA = new Group("DevJava");
+            Student studentA = new Student("Petro","Gavrilyk",25);
+            Student studentB = new Student("Semen", "Werytin", 20);
+            studentB.SetStageEvolution(StageEvolution.Demo1);
+
+            groupA.AddStudent(studentA);
+            groupA.AddStudent(studentB);
+
+            Console.Write(groupA.ConvString());
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    public struct Student : IDisposable
+    {
+        private static int quantity;
+        private String name;
+        private String lastName;
+        private int id;
+        private int age;
+        private StageEvolution stageEvolution;
+        
+
+
+        public Student(String name, String lastName, int age)
+        {
+            quantity += 1;
+            this.id = quantity;
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.stageEvolution = StageEvolution.None;
+        }
+
+        public String GetName()
+        {
+            return this.name;
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public String GetLastName()
+        {
+            return this.lastName;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        public StageEvolution GetStageEvolution()
+        {
+            return this.stageEvolution;
+        }
+
+        public void SetStageEvolution(StageEvolution stageEvolution)
+        {
+            this.stageEvolution = stageEvolution;
+        }
+
+        public void SetName(String name)
+        {
+            this.name = name;
+        }
+
+        public void SetLastName(String lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public String ConvString()
+        {
+            String str;
+            str = "ID = "+this.GetId().ToString() + ", NAME = "+this.GetName() + ", LAST_NAME = "+this.GetLastName() + ", AGE = "+this.GetAge().ToString()+ ", STAGE = "+this.GetStageEvolution();
+            return str;
+        }
+
+    }
+}
